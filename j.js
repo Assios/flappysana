639673f2ea140e74b20b58f544db16c6d8e62967
Asbjørn Steinskog <asbjorn@steinskog.me>
2017-04-08 00:20:08 +0200

    Add countdown

---
+++ j.js
@@ -0,0 +1,30 @@
+var end = new Date('04/10/2017 01:23 PM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = ' ... ';
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('k').innerHTML = days + ' ';
+    document.getElementById('k').innerHTML += hours + ' ';
+    document.getElementById('k').innerHTML += minutes + ' ';
+    document.getElementById('k').innerHTML += seconds + ' ';
+}
+
+timer = setInterval(showRemaining, 1000);
